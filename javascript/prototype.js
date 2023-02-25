234138c5e192f69b5f1c18ab42e19b3c1b29cb54
dear-ore <adorable2209@gmail.com>
2023-02-25 15:57:23 +0100

    vanilla javascript-learnt classes

---
+++ javascript/prototype.js
@@ -0,0 +1,22 @@
+class student {
+    constructor(){
+        this.name = name;
+        this.level = level;
+        this.matricNumber = matricNumber;
+    }
+
+    greetStudent() {
+        console.log(`Hello, my name is ${this.name} and I am in ${this.level} level. My reg number is ${this.matricNumber}.`);
+    }
+}
+
+
+class subStudent extends student {
+    constructor(faculty) {
+        super();
+        this.faculty = ;
+    }
+}
+// const student1 = new student("Yetunde", 200, "floe");
+// console.log(student1);
+//student1.greetStudent();
